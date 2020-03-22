b732445c03ddd127448cac7f6f9b3b69f003ecb7
yann1294 <dev.yann12@gmail.com>
2020-03-22 14:14:53 +0530

    Updates

---
+++ precache-manifest.cc7c58d14148cd35fdb8aa9d3a3ac739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053b742a1976c4441086d3ec24550d8c",
+    "url": "/corona-dashboard/index.html"
+  },
+  {
+    "revision": "5ac80170668d2c28d7c6",
+    "url": "/corona-dashboard/static/css/main.9a2d4adb.chunk.css"
+  },
+  {
+    "revision": "2f314e409d44832a3601",
+    "url": "/corona-dashboard/static/js/2.43eb54a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-dashboard/static/js/2.43eb54a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac80170668d2c28d7c6",
+    "url": "/corona-dashboard/static/js/main.422fd1f6.chunk.js"
+  },
+  {
+    "revision": "b44093c9661a4f34a709",
+    "url": "/corona-dashboard/static/js/runtime-main.8679f692.js"
+  }
+]);
